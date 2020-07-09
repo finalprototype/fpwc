4bac0e749154c8758f7bfcfc0ca12a02da6bb16c
fp <final.prototype@gmail.com>
2020-07-09 19:39:26 -0400

    [fix] port

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,9 @@
 const app = require("./app");
+const port = process.env.PORT || 8080;
 
-const server = app.listen(
-	7000,
-	() => console.info("Server running on port 7000")
-);
+app.listen(port, () => {
+  console.log('Server running at http://127.0.0.1:%s', port);
+});
 
 process.on("SIGTERM", () => {
   server.close(() => {
