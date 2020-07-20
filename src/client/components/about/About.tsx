ace500d279d2466ab59ef2dee2de1f32ae85ff63
fp <final.prototype@gmail.com>
2020-07-20 16:58:49 -0400

    [add] dummy components

---
+++ src/client/components/about/About.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from "./styles/About.scss";
+
+const About: React.FunctionComponent = () => {
+  return (
+    <div className={styles.container}>
+      About
+    </div>
+  );
+};
+
+export default About;
---
+++ src/client/components/work/Work.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from "./styles/Work.scss";
+
+const Work: React.FunctionComponent = () => {
+  return (
+    <div className={styles.container}>
+      Work
+    </div>
+  );
+};
+
+export default Work;
