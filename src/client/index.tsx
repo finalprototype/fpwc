d52ab0a028f43c00e39fbe88a4d2dba85c06af3a
fp <final.prototype@gmail.com>
2020-08-05 21:10:27 -0400

    t4

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './styles/fonts.scss';
 import './styles/base.scss';
 
 import AppContainer from './components/app/AppContainer';
